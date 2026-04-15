0065547d6a4f4cd7ce3d3d9b4c38197c95b0a7fd
Hanns <hannsgilleaux@gmail.com>
2026-04-15 05:35:54 +0100

    update son

--- fcm_test/lib/services/sound_services.dart
+++ fcm_test/lib/services/sound_services.dart
@@ -1,4 +1,6 @@
 
+import 'dart:async';
+
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
@@ -6,14 +8,20 @@ class SoundService {
   static final AudioPlayer _player = AudioPlayer();
 
   // https://mixkit.co/free-sound-effects/ ou https://freesound.org
-  static const String _sendSound    = 'sounds/send.wav';
-  static const String _receiveSound = 'sounds/receive.flac';
+  static const String _sendSound    = 'sounds/receive.mp3';
+  static const String _receiveSound = 'sounds/receive.mp3';
 
   /// Son joué quand on appuie sur "Notifier" (envoi)
   static Future<void> playSend() async {
     try {
+      await _player.setReleaseMode(ReleaseMode.loop);
       await _player.stop();
       await _player.play(AssetSource(_sendSound));
+       Timer(const Duration(seconds: 90), ()async {
+        await _player.stop();
+        await _player.dispose();
+
+       });
     } catch (e) {
       // Fallback : vibration si le fichier son est absent
       debugPrint('⚠️ Son envoi introuvable, vibration en fallback: $e');
@@ -23,9 +31,14 @@ class SoundService {
   /// Son joué quand une notification est reçue (foreground)
   static Future<void> playReceive() async {
     try {
+      await _player.setReleaseMode(ReleaseMode.loop);
       await _player.stop();
       await _player.play(AssetSource(_receiveSound));
-      debugPrint('🔊 Son réception joué');
+       Timer(const Duration(seconds: 90), ()async {
+        await _player.stop();
+        await _player.dispose();
+
+       });
     } catch (e) {
       // Fallback : vibration si le fichier son est absent
       debugPrint('⚠️ Son réception introuvable, vibration en fallback: $e');
